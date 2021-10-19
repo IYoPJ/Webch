013e70c95ac2f26eb79199aa4f647f19cd84c51f
IYoPJ <69121220+IYoPJ@users.noreply.github.com>
2021-10-19 14:54:41 +0300

    Add files via upload

---
+++ web.py
@@ -0,0 +1,33 @@
+import requests
+from time import sleep
+
+print('''
+------------------------------------------
+		Dictionary web  👣
+		+ The developer : Salman - IYoPJ +
+		
+		snapchat : IYoPJ € 
+
+------------------------------------------
+''')
+
+
+
+
+dictionary = open('dicn.txt', 'r')
+url = input('URL ---> ')
+for x in dictionary:
+	r=requests.get(url + '/' + x.rstrip('/n'))
+	if r.status_code == 200:
+		print(url + '/' + x + '[----------------Good-----------------]')
+		
+	else:
+		print(url + '/'+ x +  '[----------------Not-------------------]')
+		
+		
+		
+ff = open('Good.txt', 'w')
+ff.write(r)
+ff.cloce()
+		
+
